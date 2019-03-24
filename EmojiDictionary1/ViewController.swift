26fc332edf0e4668ac26f80bdf6194efe681937e
Salvador Garcia <salvadorgarcia@salvadors-macbook-pro.local>
2019-03-24 09:05:10 -0500

    Initial Commit

---
+++ EmojiDictionary1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiDictionary1
+//
+//  Created by Salvador Garcia on 3/24/19.
+//  Copyright © 2019 Salvador Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
